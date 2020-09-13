e00518c5d80fb0960d486cc6cad5ca60765d9b8c
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-09-13 11:57:12 +0800

    first commit

---
+++ webapp/src/app/core/admin/admin.routing.ts
@@ -0,0 +1,39 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManagementAuditComponent } from './management-audit/management-audit.component';
+import { ManagementUserComponent } from './management-user/management-user.component';
+import { ReportComponent } from './report/report.component';
+import { FaqComponent } from './faq/faq.component';
+
+export const AdminRoutes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'management',
+                children: [
+                    {
+                        path: 'audit-trails',
+                        component: ManagementAuditComponent
+                    },
+                    {
+                        path: 'user',
+                        component: ManagementUserComponent
+                    }
+                ]
+            },
+            {
+                path: 'report',
+                component: ReportComponent
+            },
+            {
+                path: 'faq',
+                component: FaqComponent
+            }
+        ]
+    }
+]
